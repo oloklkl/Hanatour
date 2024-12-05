5e4bcdbc10ea93d6c77775b1a2cbae0d106d64bd
oloklkl <sungju7694@naver.com>
2024-12-05 22:32:28 +0900

    js

--- resources/js/ui-pattern.js
+++ resources/js/ui-pattern.js
@@ -25,26 +25,3 @@ $('.lang__btn').click(function () {
 $('.lang__lst li').click(function () {
     $('.lang__lst').hide();
 });
-
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    const videoContainer = document.querySelector('.video-container');
-    const anotherSection = document.querySelector('.another-section');
-
-    const videoContainerBottom = videoContainer.offsetTop + videoContainer.offsetHeight;
-    const anotherSectionBottom = anotherSection.offsetTop + anotherSection.offsetHeight;
-
-    if (window.scrollY < videoContainerBottom) {
-        // 비디오 섹션 내에서는 배경 투명, 글자 흰색
-        header.style.backgroundColor = 'transparent';
-        header.style.color = 'white';
-    } else if (window.scrollY >= videoContainerBottom && window.scrollY < anotherSectionBottom) {
-        // anotherSection 내에서는 배경 투명, 글자 흰색
-        header.style.backgroundColor = 'transparent';
-        header.style.color = 'white';
-    } else {
-        // 두 섹션 이후에는 배경 흰색, 글자 검정
-        header.style.backgroundColor = 'white';
-        header.style.color = 'black';
-    }
-});
