15dac05bf561f4aaa99dac9bb757e61ba3f72485
oloklkl <sungju7694@naver.com>
2024-12-19 00:09:02 +0900

    js

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -31,13 +31,13 @@ showSlide(currentSlide);
 setInterval(nextSlide, 6000); // 6초마다 슬라이드 전환
 
 // sub_02 swiper
-var swiper2 = new Swiper('.mySwiper2', {
+var swiper = new Swiper('.mySwiper3', {
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
 });
-var swiper3 = new Swiper('.mySwiper3', {
+var swiper = new Swiper('.mySwiper2', {
     spaceBetween: 10,
     navigation: {
         nextEl: '.swiper-button-next',
