ef7522a4994a525aafe373ed9b6c4266704e7fe8
oloklkl <sungju7694@naver.com>
2024-12-19 17:02:47 +0900

    js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,17 +1,12 @@
 //scrolla.js
-$(function() {
-	$('.animate').scrolla({
-		mobile: true, //모바일버전시 활성화
-		once: false //스크롤시 딱 한번만 하고싶을땐 true
-	});    
-}); 
-
-
-
-
-
-//splitting.js
-$(function(){
-    Splitting();  //대문자로쓴다!!!
+$(function () {
+    $('.animate').scrolla({
+        mobile: true, //모바일버전시 활성화
+        once: false, //스크롤시 딱 한번만 하고싶을땐 true
+    });
 });
 
+//splitting.js
+$(function () {
+    Splitting(); //대문자로쓴다!!!
+});
